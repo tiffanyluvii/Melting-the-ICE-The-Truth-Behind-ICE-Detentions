4c07d65e5b1f3c6c1e605eafc371faa8bb4bb3db
tiffanyluvii <tiffanylu123901@gmail.com>
2026-04-20 23:24:04 -0400

    created the graphs for section 2. Still need to arrange the axis and consider overlapping one another

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
+// Load the CSVs and other important attributes
+const parseDate = d3.timeParse("%Y-%m-%d")
+const biden_data = await d3.csv("./data/detention-stays_filtered_biden_cleaned.csv", d => ({
+  ...d,
+  date: parseDate(d.date)
+}))
+
+const trump_data = await d3.csv("./data/detention-stays_filtered_trump_cleaned.csv", d => ({
+  ...d,
+  date: parseDate(d.date)
+}))
+
+const root = document.documentElement
+const styles = getComputedStyle(root)
+const main_red = styles.getPropertyValue('--main-red')
+
 // Section 1: Overview
 
 // hateful content collage
@@ -21,27 +37,6 @@ function showImage(id) {
   if (id != 1) images[id - 1].style.opacity = 0.5;
 }
 
-// window.addEventListener("scroll", () => {
-//   const section = document.getElementById("image-scroll");
-//   const rect = section.getBoundingClientRect();
-
-//   // Calculate progress: 0 when top is at 0, 1 when bottom reaches 0
-//   // rect.top is negative when the top of the section is above the viewport
-//   const totalScrollableHeight = section.offsetHeight - window.innerHeight;
-//   const scrollProgress = -rect.top / totalScrollableHeight;
-
-//   // Clamp progress between 0 and 1 so it doesn't trigger early or late
-//   const progress = Math.max(0, Math.min(1, scrollProgress));
-
-//   // Trigger your images based on that progress
-//   if (progress > 0.05) showImage(1);
-//   if (progress > 0.15) showImage(2);
-//   if (progress > 0.25) showImage(3);
-//   if (progress > 0.35) showImage(4);
-//   if (progress > 0.50) showImage(5);
-//   if (progress > 0.65) showImage(6);
-// });
-
 const section = document.getElementById("image-animation");
 let locked = false
 let hasRun = false
@@ -56,7 +51,7 @@ window.addEventListener("scroll", () => {
         // simulate animation time
         setTimeout(() => {
             document.body.classList.remove("no-scroll");
-        }, 30000);
+        }, 3000);
   }
 });
 
@@ -65,7 +60,7 @@ function runAnimation(){
       const id = Number(key);
       setTimeout(() => {
           showImage(id)
-      }, i * 5000);
+      }, i * 500);
     });
 }
 
@@ -80,3 +75,65 @@ const observer = new IntersectionObserver((entries) => {
 });
 
 observer.observe(section)
+
+
+// Section 2: Scale of Detention Between Presidency
+
+const width = 800;
+const height = 300;
+const chartWidth = 350;
+const chartHeight = 250;
+
+const detention_scale_svg = d3.select('#detention-scale-vis').append("svg").attr("width", width).attr("height", height);
+const biden_scale_g = detention_scale_svg.append("g").attr("transform", "translate(40,20)");
+const trump_scale_g = detention_scale_svg.append("g").attr("transform", "translate(440,20)");
+
+const biden_detention_map = d3.rollups(
+  biden_data,
+  v=>v.length,
+  d=>d3.timeMonth(d.date)
+).map(
+  (([date, count]) => ({
+    date,
+    count
+  }))
+).sort((a,b) => a.date - b.date)
+
+const biden_x_scale = d3.scaleTime().domain(d3.extent(biden_detention_map, d=>d.date)).range([0, chartWidth]);
+const biden_y_scale = d3.scaleLinear().domain([0,d3.max(biden_detention_map, d=>d.count)]).range([chartHeight, 0]);
+const biden_line = d3.line()
+  .x(d=>biden_x_scale(d.date))
+  .y(d=>biden_y_scale(d.count));
+
+biden_scale_g.append("path")
+  .datum(biden_detention_map)
+  .attr("fill", "none")
+  .attr("stroke", main_red)
+  .attr("stroke-width", 2)
+  .attr("d", biden_line);
+
+const trump_detention_map = d3.rollups(
+  trump_data,
+  v=>v.length,
+  d=>d3.timeMonth(d.date)
+).map(
+  (([date, count]) => ({
+    date, 
+    count
+  }))
+).sort((a,b) => a.date - b.date);
+
+console.log(trump_detention_map)
+
+const trump_x_scale = d3.scaleTime().domain(d3.extent(trump_detention_map, d=>d.date)).range([0, chartWidth]);
+const trump_y_scale = d3.scaleLinear().domain([0, d3.max(trump_detention_map, d=>d.count)]).range([chartHeight, 0]);
+const trump_line = d3.line()
+  .x(d=>trump_x_scale(d.date))
+  .y(d=>trump_y_scale(d.count));
+
+trump_scale_g.append("path")
+  .datum(trump_detention_map)
+  .attr("fill", "none")
+  .attr("stroke", main_red)
+  .attr("stroke-width", 2)
+  .attr("d", trump_line);
--- miscellaneous/filter.py
+++ miscellaneous/filter.py
@@ -1,16 +1,25 @@
 import pandas as pd
-biden_df = pd.read_csv('./data/detention-stays_filtered_2022_2023_2024.csv')
-trump_df = pd.read_csv('./data/detention-stays_filtered_2025_2026.csv')
+biden_df = pd.read_csv('./data/detention-stays_filtered_biden_cleaned.csv')
+trump_df = pd.read_csv('./data/detention-stays_filtered_trump_cleaned.csv')
 columns_to_keep = ['stay_ID', 'stay_book_in_date_time', 'stay_book_out_date_time', 'gender', 'ethnicity', 'birth_country', 'citizenship_country', 'known_terrorist_yes_no', 
                    'suspected_gang_yes_no', 'msc_charge', 'felon', 'case_status', 'case_category', 'final_order_yes_no', 'departure_country', "msc_crime_class"]
 
 biden_df = biden_df[columns_to_keep]
 biden_df.dropna()
-biden_df["year"] = biden_df["stay_ID"].str.extract(r'_(\d{4})').astype(int)
+biden_df["date"] = pd.to_datetime(
+    biden_df["stay_ID"].str.extract(r'_(\d{4}-\d{2}-\d{2})')[0]
+)
+biden_df["year"] = biden_df["date"].dt.year
+biden_df["month"] = biden_df["date"].dt.month
+
 
 trump_df = trump_df[columns_to_keep]
 trump_df.dropna()
-trump_df["year"] = trump_df["stay_ID"].str.extract(r'_(\d{4})').astype(int)
+trump_df["date"] = pd.to_datetime(
+    trump_df["stay_ID"].str.extract(r'_(\d{4}-\d{2}-\d{2})')[0]
+)
+trump_df["year"] = trump_df["date"].dt.year
+trump_df["month"] = trump_df["date"].dt.month
 
 print(biden_df.head(5))
 print(trump_df.head(5))
